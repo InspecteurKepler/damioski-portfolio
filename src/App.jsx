f8317ab47706f0cfbd14ffd7196d7ad8b771d379
DAMIOSKIDEV <45036197+DAMIOSKIDEV@users.noreply.github.com>
2024-11-09 13:28:39 +0100

    Base conf and website
    Website not designed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.scss';
+
+function App() {
+
+  return (
+    <div>
+      <section>DAMIOSKI</section>
+      <section>Cursus</section>
+      <section>Jobs</section>
+      <section>Skills</section>
+      <section>Loisirs</section>
+      <section>Contact</section>
+
+    </div>
+  )
+}
+
+export default App
