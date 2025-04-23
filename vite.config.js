9b7c93a868a5b5728a7d5aa45013295fe7a6ecd2
DAMIOSKIDEV <45036197+DAMIOSKIDEV@users.noreply.github.com>
2025-04-23 18:00:40 +0200

    Change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 4545,
+    host: true,
+    allowedHosts: ['damioski.de', 'portfolio.damioski.de'],
+  },
   plugins: [
     react(),
   ],
